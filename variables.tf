876a3d9cb6e27931f55a54f3484b2dedbbb24814
Matthijs Baas <matthijs.baas@sogeti.com>
2025-06-22 09:16:35 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "resource_group_name" {
+  type = string
   default = "rgDNS"
 }
 
 variable "location" {
+  type = string
   default = "Sweden Central"
 }
 
 variable "admin_username" {
+  type = string
   default = "vmdns_admin"
 }
